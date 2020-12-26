82647c6f939f72a0e038390d086cbc41f44239ec
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2020-12-26 15:30:49 +0530

    Added Education Field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fname lname dob image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fname lname dob education image])
     devise_parameter_sanitizer.permit(:account_update, keys:
-                                      %i[fname lname dob image])
+                                      %i[fname lname dob education image])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_074451) do
+ActiveRecord::Schema.define(version: 2020_12_26_094935) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_12_26_074451) do
     t.date "dob"
     t.string "google_token"
     t.string "google_refresh_token"
+    t.string "education"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
